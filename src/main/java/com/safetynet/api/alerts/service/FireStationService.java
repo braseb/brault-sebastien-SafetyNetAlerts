eee12b51ecaf668cbd4039c107eefd2134deca86
Brault sebastien <braseb37@gmail.com>
2025-05-03 14:28:51 +0200

    append @Service annotation and create getPersonByName fonction in class PersonService

--- src/main/java/com/safetynet/api/alerts/service/FireStationService.java
+++ src/main/java/com/safetynet/api/alerts/service/FireStationService.java
@@ -1,5 +1,8 @@
 package com.safetynet.api.alerts.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FireStationService {
 
 }
--- src/main/java/com/safetynet/api/alerts/service/PersonService.java
+++ src/main/java/com/safetynet/api/alerts/service/PersonService.java
@@ -1,5 +1,23 @@
 package com.safetynet.api.alerts.service;
 
-public class PersonService {
+import java.io.IOException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.safetynet.api.alerts.model.Person;
+import com.safetynet.api.alerts.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	private PersonRepository personRepository;
+	
+	public List<Person> getPersonByName(String lastName, String firstName) throws Exception{
+		
+		return personRepository.getPersonByName(lastName, firstName);
+		
+	}
 }
