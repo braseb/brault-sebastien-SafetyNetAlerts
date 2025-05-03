924da399e6703bd3e7246545af296e8e3cd7a4d9
Brault sebastien <braseb37@gmail.com>
2025-05-03 14:18:46 +0200

    implement CommandLineRunner for tests

--- src/main/java/com/safetynet/api/alerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/api/alerts/SafetyNetAlertsApplication.java
@@ -1,13 +1,23 @@
 package com.safetynet.api.alerts;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.safetynet.api.alerts.repository.PersonRepository;
+
 @SpringBootApplication
-public class SafetyNetAlertsApplication {
+public class SafetyNetAlertsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//System.out.println(new PersonRepository().getPersonByName("Boyd", "John"));
+		
 	}
 
 }
