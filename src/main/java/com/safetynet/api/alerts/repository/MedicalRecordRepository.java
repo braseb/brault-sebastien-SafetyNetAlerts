206dc3e2ce107bb0936e407f91eeacf8aeacb74a
Brault sebastien <braseb37@gmail.com>
2025-05-03 14:33:58 +0200

    implement some fonctions for test behavior for repository class

--- src/main/java/com/safetynet/api/alerts/repository/MedicalRecordRepository.java
+++ src/main/java/com/safetynet/api/alerts/repository/MedicalRecordRepository.java
@@ -1,5 +1,44 @@
 package com.safetynet.api.alerts.repository;
 
-public class MedicalRecordRepository {
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
+import com.safetynet.api.alerts.datas.JsonDatas;
+import com.safetynet.api.alerts.model.MedicalRecord;
+
+
+@Component
+public class MedicalRecordRepository {
+	
+	@Autowired
+	private JsonDatas datas;
+	
+	public MedicalRecordRepository() {
+		//gson = new Gson();
+		//JsonReader reader = new JsonReader(new FileReader(fileJsonPath));
+		//fileCache = JsonParser.parseReader(reader).getAsJsonObject();
+	}
+	
+	
+	public List<MedicalRecord> getAllMedicalRecord() throws IOException {
+		JsonArray medicalRecordsArray = datas.getFileCache().getAsJsonArray("medicalrecords");
+		List<MedicalRecord> medicalRecords = Collections.emptyList();
+		if (medicalRecordsArray != null) {
+			Gson gson = new Gson();
+			Type typeListMedicalRecord = new TypeToken<List<MedicalRecord>>() {}.getType();
+			medicalRecords = gson.fromJson(medicalRecordsArray, typeListMedicalRecord);
+		}
+		
+				
+		return medicalRecords;
+	}
 }
--- src/main/java/com/safetynet/api/alerts/repository/PersonRepository.java
+++ src/main/java/com/safetynet/api/alerts/repository/PersonRepository.java
@@ -1,5 +1,81 @@
 package com.safetynet.api.alerts.repository;
 
-public class PersonRepository {
 
+
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
+import com.safetynet.api.alerts.datas.JsonDatas;
+import com.safetynet.api.alerts.model.Person;
+
+@Component
+public class PersonRepository {
+	
+	@Autowired
+	private JsonDatas datas;
+	
+	public PersonRepository() {
+		
+	}
+	
+	public List<Person> getPersonByName(String lastName, String firstName) throws IOException {
+		JsonArray personArray = datas.getFileCache().getAsJsonArray("persons");
+		
+		List<Person> personsSelect = Collections.emptyList();
+		
+		if (personArray != null){
+			Gson gson = new Gson();
+			Type personsListType = new TypeToken<List<Person>>() {}.getType();
+			List<Person> persons = gson.fromJson(personArray, personsListType);
+			personsSelect =	persons.stream()
+									.filter(p-> p.getFirstName().equals(firstName) && p.getLastName().equals(lastName))
+									.collect(Collectors.toList());
+		}	
+		
+		
+		return personsSelect;
+	}
+		
+	public List<Person> getPersonByAddress(String address) throws IOException {
+		JsonArray personArray = datas.getFileCache().getAsJsonArray("persons");
+		List<Person> personsSelect = Collections.emptyList();
+		
+		if (personArray != null){
+			Gson gson = new Gson();
+			Type personsListType = new TypeToken<List<Person>>() {}.getType();
+			List<Person> persons = gson.fromJson(personArray, personsListType);
+			personsSelect =	persons.stream()
+									.filter(p-> p.getAddress().equals(address))
+									.collect(Collectors.toList());
+		}
+		
+		
+		
+		return personsSelect;
+}
+	
+	public List<Person> getAllPerson() throws IOException {
+		JsonArray personArray = datas.getFileCache().getAsJsonArray("persons");
+		List<Person> persons = Collections.emptyList();
+		
+		if (personArray != null){
+			Gson gson = new Gson();
+			Type personsListType = new TypeToken<List<Person>>() {}.getType();
+			persons = gson.fromJson(personArray, personsListType);
+		}
+				
+		return persons;
+	}
+	
+	
 }
